9aa947be0e1bb16b14f41f654c16412339e1e923
slic-one <slic1one@gmail.com>
2017-01-07 23:13:08 +0200

    Regex version (Python 3.5.2)

--- name2lalo_regex.py
+++ name2lalo_regex.py
@@ -1,55 +1,46 @@
 import sys
-import httplib
+import http.client
 import json
+import re
+import codecs
 
-is_latitude = False
-is_longitude = False
 
-is_coordinates = False
-res_data = []
-
-la = ""
-lo = ""
+class ParseError(Exception):
+    def __init__(self, message):
+        self.message = message
 
 
 def parse_wiki(city):
-    global la
-    global lo
 
-    connection = httplib.HTTPSConnection('en.wikipedia.org')
+    connection = http.client.HTTPSConnection("en.wikipedia.org")
     connection.request("GET", "/wiki/" + city)
 
-    # Clear values:
-    la = ""
-    lo = ""
-
-    try:
-        response = connection.getresponse()
-    except httplib.ResponseNotReady:
-        return -1
+    response = connection.getresponse()
 
     if response.status != 200:
-        print city + ': Page not found.'
+        print(city + ': Page not found.')
         return -1
 
     # Some encodings trigger UnicodeDecodeError on feed()
     page = response.read().decode('utf-8')
 
-    # TODO get data here
-
-    # TODO get data here
+    # REGEX
+    la = re.search('latitude">.{8,12}</span', page)
+    lo = re.search('longitude">.{8,12}</span', page)
 
     connection.close()
 
-    if la == "" or lo == "":
-        print city + ": No coordinates found on this page"
+    if la is not None:
+        la = la.group(0).split(">")[1].split("<")[0]
+        lo = lo.group(0).split(">")[1].split("<")[0]
+        return {
+            'name': city,
+            'la': la,
+            'lo': lo,
+        }
+    else:
+        print(city + ": No coordinates found on this page.")
         return -1
-
-    return {
-        'name': city,
-        'la': la,
-        'lo': lo,
-    }
 
 
 # Try getting command line argument, if none - ask for input:
@@ -57,25 +48,29 @@ file_name = ""
 try:
     file_name = sys.argv[1]
 except IndexError:
-    file_name = raw_input('Please input city name: ')
+    file_name = input('Input file name: ')
+# file_name = "in.json" # tmp
 
 # Read input .json file:
 try:
     with open(file_name) as data_file:
         city_names = json.load(data_file)
 except IOError:
-    print "Couldn't parse the file"
+    print("Couldn't parse the file")
     quit(1)
 
-output_file = open('out.json', 'w')
+try:
+    output_file = sys.argv[1]
+except IndexError:
+    output_file = input('Output file name: ')
+
 output_data = []
 # Write output:
 for index in range(len(city_names)):
     result = parse_wiki(city_names[index]["name"])
     if result != -1:
         output_data.append(result)
-        print result['name'] + ": " + result['la'] + ", " + result['lo']
+        print(result['name'] + ": " + result['la'] + ", " + result['lo'])
 
-json.dump(output_data, output_file, indent=4, sort_keys=True, ensure_ascii=True)
-
-output_file.close()
+with codecs.open(output_file, 'w', encoding='utf-8') as output:
+    json.dump(output_data, output, indent=4, sort_keys=True, ensure_ascii=False)
